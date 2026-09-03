b1802426839bac63e10fa53e7fee3264867721c4
Le Huong Giang <hgiangle.work@gmail.com>
2026-09-03 22:12:15 +0700

    go

--- src/data/guests.js
+++ src/data/guests.js
@@ -18,8 +18,6 @@ export const snapshot = [
     slug: "mr-nguyen-van-an",
     name: "Mr. Nguyễn Văn An",
     code: "001",
-    attending: true,
-    table: "10",
     seats: 2,
     lang: "en",
   },
